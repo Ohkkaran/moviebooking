359b59f65ad4fba38c492b513478c1595f623e2f
Karan Kumar <karankr828@gmail.com>
2024-07-17 15:37:59 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors")
+const app = express();
+require("dotenv").config();
+const dbConfig = require("./config/dbConfig");
+
+app.use(express.json());
+app.use(cors({
+    credentials:true
+}));
+
+const usersRoute = require("./routes/usersRoute");
+const moviesRoute = require("./routes/moviesRoute");
+const theatresRoute = require("./routes/theatresRoute");
+const bookingsRoute = require("./routes/bookingsRoute");
+
+
+app.use("/api/users", usersRoute);
+app.use("/api/movies", moviesRoute);
+app.use("/api/theatres", theatresRoute);
+app.use("/api/bookings", bookingsRoute);
+
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () =>
+    console.log(`Node JS Server is running on port ${port}`)
+  );
+
+
+
